caf2f37092604bd4c7ca2692a2f706bc35c25a4d
Keloysius Mak <sius_max@hotmail.com>
2017-09-06 13:12:26 +0800

    Add space

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -97,7 +97,7 @@ public class AddressBook {
     private static final String COMMAND_ADD_DESC = "Adds a person to the address book.";
     private static final String COMMAND_ADD_PARAMETERS = "NAME "
             + PERSON_DATA_PREFIX_PHONE + "PHONE_NUMBER "
-            + PERSON_DATA_PREFIX_EMAIL + "EMAIL"
+            + PERSON_DATA_PREFIX_EMAIL + "EMAIL "
             + PERSON_DATA_PREFIX_AGE + "AGE";
     private static final String COMMAND_ADD_EXAMPLE = COMMAND_ADD_WORD + " John Doe p/98765432 e/johnd@gmail.com a/18";
 
